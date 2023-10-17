c63ff4a7e57e3aaa5fe1d225964c7b8c0b0a9444
Daniel Ramirez <danidehorta544@gmail.com>
2023-10-16 18:02:21 -0600

    url

--- src/app.js
+++ src/app.js
@@ -15,6 +15,9 @@ app.use(cors({
     origin:[
         'http://localhost:5173',
         'http://127.0.0.1:5173',
+        "http://localhost",
+        "http://localhost/productos",
+        "https://frontend-apvj.onrender.com",
     ],
     credentials: true
 }));
